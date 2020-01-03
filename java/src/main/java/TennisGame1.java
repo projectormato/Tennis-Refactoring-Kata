d371b069955a89084d7781d221b6c84701ae59ca
projectormato <k.keita811.anuy@gmail.com>
2020-01-03 21:57:23 +0900

    refactor: コンストラクタで渡されたプレイヤー名を使用するように変更

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -12,7 +12,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
+        if (playerName.equals(this.player1Name))
             m_score1 += 1;
         else
             m_score2 += 1;
@@ -39,10 +39,10 @@ public class TennisGame1 implements TennisGame {
             }
         } else if (m_score1 >= 4 || m_score2 >= 4) {
             int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = new StringBuilder("Advantage player1");
-            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
-            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
-            else score = new StringBuilder("Win for player2");
+            if (minusResult == 1) score = new StringBuilder("Advantage " + this.player1Name);
+            else if (minusResult == -1) score = new StringBuilder("Advantage " + this.player2Name);
+            else if (minusResult >= 2) score = new StringBuilder("Win for " + this.player1Name);
+            else score = new StringBuilder("Win for " + this.player2Name);
         } else {
             for (int i = 1; i < 3; i++) {
                 if (i == 1) tempScore = m_score1;
