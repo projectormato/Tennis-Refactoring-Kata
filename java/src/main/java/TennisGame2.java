19cfe095139ab45b7e39bf5b640f8f644a7748cb
projectormato <k.keita811.anuy@gmail.com>
2020-01-24 21:15:48 +0900

    refactor: Game2 publicの必要がない変数をprivateにした

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -1,12 +1,11 @@
 
 public class TennisGame2 implements TennisGame {
-    public int P1point = 0;
-    public int P2point = 0;
-
-    public String P1res = "";
-    public String P2res = "";
     private String player1Name;
     private String player2Name;
+    private int P1point;
+    private int P2point;
+    private String P1res;
+    private String P2res;
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
